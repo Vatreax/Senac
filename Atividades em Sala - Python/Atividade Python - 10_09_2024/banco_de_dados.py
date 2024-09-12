9aa58b0312d876300ce83ca757055834509572a1
Vatreax <rafasmontiel13@gmail.com>
2024-09-12 07:17:28 -0400

    Escreva

--- Atividades em Sala - Python/Atividade Python - 10_09_2024/banco_de_dados.py
+++ Atividades em Sala - Python/Atividade Python - 10_09_2024/banco_de_dados.py
@@ -12,9 +12,14 @@ import mysql.connector
 from tkinter import *
 from tkinter import messagebox
 
+global usuario,senha,cursor,dataBase
+
 def logar():
+    global usuario, senha
     usuario = usuario_entry.get()
     senha = senha_entry.get()
+
+
     
     if usuario and senha:
         if verificar_login(usuario, senha):
@@ -23,10 +28,31 @@ def logar():
             inserir_texto.title("Inserir Texto")
             inserir_texto.geometry('300x300')
 
+            def escrever():
+                escreva = texto_entry.get()
+                sql = f"UPDATE login SET texto = '{escreva}' WHERE usuario = '{usuario} and senha = {senha}'"
+
+                cursor.execute(sql)
+                dataBase.commit()
+                
+
+                myresult = cursor.fetchall()
+
+                for x in myresult:
+                    print(x)
+                    
+                print("Query Excecuted successfully")
+                dataBase.close()
+
             texto_label = Label(inserir_texto, text="Texto", font=('arial',15,'bold'), bg='white')
             texto_label.place(relx=0.50, rely=0.25, anchor='center')
             texto_entry = Entry(inserir_texto, font=('arial',15), bg='white')
             texto_entry.place(relx=0.50, rely=0.35, anchor='center')
+
+            escreve_button = Button(inserir_texto, text="Escrever", font=('arial',15,'bold'), bg='white', command=escrever)
+            escreve_button.place(relx=0.50, rely=0.48, anchor='center')
+
+
 
             inserir_texto.mainloop()
         else:
@@ -51,13 +77,11 @@ def verificar_login(usuario, senha):
         resultado = cursor.fetchone()
         
         cursor.close()
-        dataBase.close()
         
         return resultado is not None
 
     except mysql.connector.Error:
         print(f"Erro","Login Fracassado")
-
 
 
 registros = Tk()
