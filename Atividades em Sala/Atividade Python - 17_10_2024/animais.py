20df4766730345508b7a619009210af24d978a98
Vatreax <rafasmontiel13@gmail.com>
2024-10-17 11:07:23 -0400

    animais

---
+++ Atividades em Sala/Atividade Python - 17_10_2024/animais.py
@@ -0,0 +1,7 @@
+class Animal:
+    def __init__(self, nome, nome_cientifico, nutricao, habitat):
+        self.nome = nome
+        self.nome_cientifico = nome_cientifico
+        self.nutricao = nutricao
+        self.habitat = habitat
+
