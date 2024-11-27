e207cc296f9805035c3fd52d263f9b7ff55343b0
Vatreax <rafasmontiel13@gmail.com>
2024-11-27 11:15:47 -0400

    NPU

--- Atividades em Sala/JS/Atividade JS - 27_11_2024/Atividade/script.js
+++ Atividades em Sala/JS/Atividade JS - 27_11_2024/Atividade/script.js
@@ -1,24 +1,38 @@
-const errorSpan = document.querySelector('.span-erro');
-const successSpan = document.querySelector('.span-deu-certo');
+const input1 = document.querySelectorAll('input');
+const input2 = document.querySelector('.input2');
+const input3 = document.querySelector('.input3');
 
-const npu_1parte = /^({0-9}{7}\-[0-9]{2}\.{0-9}{4}\$/;
-const npu_2parte = /^({0-9}{1,3}\$/;
-const npu_3parte = /^({0-9}{4}\$/;
+const npu_1parte = /^({0-9}{7}\-[0-9]{2}\.{0-9}{4}$/;
+const npu_2parte = /^({0-9}{1,3}$/;
+const npu_3parte = /^({0-9}{4}$/;
 
-function digitado(index){
-    spans[index].style.display = 'block',
-    spans[index].style.color = red
-}
 
-function removeError(index){
-    spans[index].style.display = 'none';
-}
-
-function npuValidate(){
-    if (npu.test(inputs[0].value)) {
-    removeError(0);
-}
-    else{
-        setError(0);
+function digitado(){
+    if (npu_1parte.test(input1.value)) {
+        removeError();
+    } else{
+        setError();
     }
+    if (npu_2parte.test(input2.value)) {
+        removeError();
+    } else{
+        setError();
+    }
+    if (npu_3parte.test(input3.value)) {
+        removeError();
+    } else{
+        setError();
+    }
+}
+
+function setError() {
+    errorSpan.style.display = 'block';
+    errorSpan.style.color = 'red';
+    successSpan.style.display = 'none';
+}
+ 
+function removeError() {
+    errorSpan.style.display = 'none';
+    successSpan.style.display = 'block';
+    successSpan.style.color = 'green'
 }
