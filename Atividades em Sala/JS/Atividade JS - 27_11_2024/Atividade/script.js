c29c8029f1658fcc0ffb18cc31fec5cccb167f6b
unknown <rafasmontiel13@gmail.com>
2024-11-27 22:36:33 -0400

    NPU

--- Atividades em Sala/JS/Atividade JS - 27_11_2024/Atividade/script.js
+++ Atividades em Sala/JS/Atividade JS - 27_11_2024/Atividade/script.js
@@ -1,38 +1,18 @@
-const input1 = document.querySelectorAll('input');
-const input2 = document.querySelector('.input2');
-const input3 = document.querySelector('.input3');
+function validarInput() {
+  const digite = document.getElementById('digite');
+  const messageSpan = document.getElementById('message');
+  
+  const npu = /^\d{7}-\d{1}\.\d{2}\.\d{4}$/;
 
-const npu_1parte = /^({0-9}{7}\-[0-9]{2}\.{0-9}{4}$/;
-const npu_2parte = /^({0-9}{1,3}$/;
-const npu_3parte = /^({0-9}{4}$/;
-
-
-function digitado(){
-    if (npu_1parte.test(input1.value)) {
-        removeError();
-    } else{
-        setError();
-    }
-    if (npu_2parte.test(input2.value)) {
-        removeError();
-    } else{
-        setError();
-    }
-    if (npu_3parte.test(input3.value)) {
-        removeError();
-    } else{
-        setError();
-    }
-}
-
-function setError() {
-    errorSpan.style.display = 'block';
-    errorSpan.style.color = 'red';
-    successSpan.style.display = 'none';
-}
- 
-function removeError() {
-    errorSpan.style.display = 'none';
-    successSpan.style.display = 'block';
-    successSpan.style.color = 'green'
+  const value = digite.value;
+  
+  if (npu.test(value)) {
+    messageSpan.textContent = 'Valor válido!';
+    messageSpan.classList.remove('message');
+    messageSpan.classList.add('valid');
+  } else {
+    messageSpan.textContent = 'Valor inválido!';
+    messageSpan.classList.remove('valid');
+    messageSpan.classList.add('message');
+  }
 }
