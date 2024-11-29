5148480eae03e78314cdb37dbc74197855e9087c
unknown <rafasmontiel13@gmail.com>
2024-11-29 05:40:51 -0400

    Alterações

--- Avaliações/Olha Aqui Caique/main.py
+++ Avaliações/Olha Aqui Caique/main.py
@@ -9,33 +9,14 @@ from views.login import Login_MainWindow
 from views.usuario import Cadastro_Usuario
 
 
-
-
-
-while True: 
-    op = input("""Digite uma das opções
-        1 - Login
-        2 - Cadastrar Usuario
-        3 - Cadastrar Livro
-        4 - Empréstimo
-        0 - Sair""")
     
 
 
-    if op == "1":
-        if __name__ == '__main__':
+
+if __name__ == '__main__':
             app = QApplication(sys.argv)
             window = Login_MainWindow()
             window.show()
             sys.exit(app.exec_())
 
-        tela = Login_MainWindow()
-
-    if op == "2":
-        if __name__ == '__main__':
-            app = QApplication(sys.argv)
-            window = Cadastro_Usuario()
-            window.show()
-            sys.exit(app.exec_())
-
-        tela = Cadastro_Usuario()
+tela = Login_MainWindow()
